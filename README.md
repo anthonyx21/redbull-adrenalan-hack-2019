63d184c548a0d4e9a18c3c7b88256a495faff2c4
Knit Admin <knitadmin@Knits-MacBook-Pro-2.local>
2019-12-08 10:37:27 -0500

    Commit skeleten code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # redbull-adrenalan-hack-2019
+
+
+Uses Twitch Messaging Interface to receive chat messages.
+
+Messages will need to be connected to a NLP model that is catered to streaming vocabulary to reflect sentiment and also connect to AR avartar.
+
---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const tmi = require('tmi.js');
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: process.env.BOT_USERNAME,
+    password: process.env.OAUTH_TOKEN
+  },
+  channels: [
+    process.env.CHANNEL_NAME
+  ]
+};
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // print command
+    console.log(`* Chat message ${commandName}`);
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
